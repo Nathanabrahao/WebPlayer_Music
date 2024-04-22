c833c2a7c0ba347df86cf0226dc301e92126dd0c
Nathanabrahao <nathanntc6@gmail.com>
2024-04-22 16:12:00 -0300

    first commit

---
+++ src/Assests/Data/infos.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Songs = [
+    {
+        id: 1,
+        name: 'Achin Soul',
+        artist: '',
+        cover: require('../images/Aching soul.jpeg'),
+        audio: require('../musics/Aching Soul.mp3')
+    },
+    {
+        id: 2,
+        name: 'Sabonete voador',
+        artist: '',
+        cover: require('../images/sabonete_voador.jpeg'),
+        audio: require('../musics/Sabonete.mp3')
+    } 
+]
+
+export default Songs;
---
+++ src/Components/Card.tsx
@@ -0,0 +1,67 @@
+import React, { FC } from "react";
+import App from "../App";
+import '../Assests/css/card.css';
+import Songs from '../Assests/Data/infos';
+
+interface CardProps {
+    musicNumber: number;
+    setMusicNumber: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const Card: FC<{ props: CardProps }> = ({ props: { musicNumber, setMusicNumber } }) => {
+    console.log(Songs)
+    return (
+        <div className="card">
+            <div className="nav">
+                <i className="material-icons">expand_more</i>
+                <span>Now Playing {musicNumber + 1}/{Songs.length} </span>
+                <i className="material-icons">queue_music</i>
+            </div>
+
+
+            <div className="img">
+                <img src={Songs[musicNumber].cover} alt="" />
+            </div>
+
+            <div className="details">
+                <p className="title">{Songs[musicNumber].name}</p>
+            </div>
+
+            <div className="progress">
+                <input type="range" min={0} max={100} />
+            </div>
+
+            <div className="timer">
+                <span>00:00</span>
+                <span>03:16</span>
+            </div>
+
+            <div className="controls">
+                <i className="material-icons">repeat</i>
+
+                <i className="material-icons">skip_previous</i>
+
+                <div className="play">
+                    <i className="material-icons">play_arrow</i>
+                </div>
+
+                <i className="material-icons" id="next">skip_next</i>
+
+                <i className="material-icons">volume_up</i>
+
+                <div className="volume">
+                    <i className="material-icons">volume_up</i>
+                    <input type="range" min={0} max={100} />
+                    <span>50</span>
+                </div>
+
+            </div>
+
+
+            <audio src={Songs[musicNumber].audio} hidden></audio>
+
+        </div>
+    );
+}
+
+export default Card;
